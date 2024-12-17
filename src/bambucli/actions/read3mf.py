7c767620461c055f886e34bd60843d07238e0f64
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-12-17 16:52:16 +0100

    3mf - Allow for multiple / no plates (#13)

--- src/bambucli/actions/read3mf.py
+++ src/bambucli/actions/read3mf.py
@@ -13,10 +13,15 @@ def read_3mf_file(args):
         spinner.task_complete()
 
         print(f"\nModel: {project_file.model.value}")
-        print(f"Nozzle Diameter: {project_file.nozzle_diameter}")
-        print(f"Filament Type: {project_file.filament_type.value}")
-        print(f"Filament Amount: {project_file.filament_amount_grams}g")
-        print(f"Print Time: {strfdelta(
-            project_file.print_time, '{H:02}:{M:02}:{S:02}')}")
+        print(f"Nozzle Diameter: {project_file.nozzle_diameter}mm")
+        if len(project_file.plates) == 0:
+            print("No sliced plates found in the project file")
+        else:
+            for plate in project_file.plates:
+                print(f"\nPlate {plate.index}")
+                print(f"Filament Type: {plate.filament_type.value}")
+                print(f"Filament Amount: {plate.filament_amount_grams}g")
+                print(f"Print Time: {strfdelta(
+                    plate.print_time, '{H:02}:{M:02}:{S:02}')}")
     except Exception as e:
         spinner.task_failed(e)
